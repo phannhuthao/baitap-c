fa021b6fe8f17c194db2da0910d6d529089eb369
phannhuthao <haophannhut26@gmail.com>
2023-12-08 22:59:11 +0700

    feature/812

---
+++ session 5/baitap5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main () {
+    int moth;
+    int day;
+    printf("Enter a day: ");
+    scanf("%d",&day);
+    if (0 < day <= 31);
+    printf("Enter a moth: ");
+    scanf("%d",&moth);
+    switch (moth) {
+        case 1:
+            printf("ma ket");
+            break;
+        case 2:
+            printf("bao binh");
+            break;
+        case 3:
+            printf("song ngu");
+            break;
+        case 4:
+            printf("bach duong");
+            break;
+        case 5:
+            printf("kim nguu");
+            break;
+        case 6:
+            printf("song tu");
+            break;
+        case 7:
+            printf("cu giai");
+            break;
+        case 8:
+            printf("su tu");
+            break;
+        case 9:
+            printf("xu nu");
+            break;
+        case 10:
+            printf("thien binh");
+            break;
+        case 11:
+            printf("bo cap");
+            break;
+        case 12:
+            printf("nhan ma");
+            break;
+        case 13:
+            printf("loi");
+
+    }
+}
