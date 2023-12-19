6a5df26de4d6d1dfd29c1c74d0ec8a20c10c1811
phannhuthao <haophannhut26@gmail.com>
2023-12-19 13:24:06 +0700

    change code

--- PTIT_KS2023A_PhanNhutHao_de1.c
+++ PTIT_KS2023A_PhanNhutHao_de1.c
@@ -1,159 +1,166 @@
-#include <stdio.h>
-#define Phan_tu 100
-
-// lúc mà chạy chương trình thì thầy cô chọn lựa chọn nào thì lướt lên 1 tí tại vì lúc nó hiện kết quả mà mấy cái lựa chọn khác nó từ 1 đến 9 nó xuất hiện sau khi có kết quả nên kết quá sẽ ở phía trên mấy cái lựa chọn
-
-int arr[Phan_tu],m,n;
-int a[100];
-int cout = 0;
-
-void Nhapgiatrin() {   // case 1
-    int n;
-    printf("Nhap so luong phan tu muon nhap vao (khong qua 100 phan tu): ");
-    scanf("%d", &n);
-
-    if (n > 0 && n <= Phan_tu) {
-        printf("Nhap gia tri cho mang: \n");
-        for (int i = 0; i < n; ++i) {
-            printf("arr[%d] = ", cout);
-            scanf("%d",&arr[cout]);
-            cout ++;
-        }
-        printf("Da nhap mang thanh cong: \n");
-    } else {
-        printf("So luong phan tu khong hop le: ");
-    }
+#include<stdio.h>
+void swap(int *ax, int *by)
+{
+    int temp = *ax;
+    *ax = *by;
+    *by = temp;
 }
-
-
-void ingiatri() {  // case 2
-    printf("Cac phan tu trong mang la:\n");
-    for (int i = 0; i < cout; ++i) {
-        printf("%d ",arr[i]);
+void bubbleShort(int a[], int n)
+{
+    for(int i=0; i<n-1; i++)
+        for(int j=0; j<n-1; j++)
+        {
+            if (a[j] < a[j+1])
+                swap(&a[j], &a[j+1]);
+        }
+}
+void array_tent(int a[], int size)
+{
+    for (int i=0; i<size; i++)
+    {
+        printf("%d ", a[i]);
     }
     printf("\n");
 }
-
-void averagePositive() { // case 3
-    int sum = 0, positiveCount = 0;
-    for (int i = 0; i < cout; ++i) {
-        if (arr[i] > 0) {
-            sum += arr[i];
-            positiveCount++;
-        }
-    }
-    if (positiveCount > 0) {
-        float average = (float) sum / positiveCount;
-        printf("Trung binh cac phan tu duong trong mang la: %.2f\n", average);
-    } else {
-        printf("Khong co phan tu duong trong mang\n");
-    }
-}
-
-void giatrik(int k) { // case 4
-    printf("Cac vi tri co gia tri bang %d trong mang la:\n", k);
-    int found = 0;
-    for (int i = 0; i < cout; ++i) {
-        if (arr[i] == k) {
-            printf("%d ", i);
-            found = 1;
-        }
-    }
-    if (!found) {
-        printf("Khong tim thay gia tri %d trong mang!\n", k);
-    }
-    printf("\n");
-}
-
-int is_prime(int num) { // case 6
-    if (num < 2) return 0;
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) return 0;
-    }
-    return 1;
-}
-
-int count_primes(float arr[], int n) {
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (is_prime((int)arr[i])) {
-            count++;
-        }
-    }
-    return count;
-}
-
-
-void chenSo(int m)//case 8
+void chenSo(int a[], int *n, int key)
 {
     int i;
-    for( i=n-1;(i>=0&&a[i]>m);i--){
-        a[i+1]=a[i];
+    if (key > a[0])
+    {
+        for (i = *n - 1; i >=0; i--)
+        {
+            a[i+1] = a[i];
+        }
+        a[0] = key;
+    }else
+    {
+        for(i=1; i<*n; i++)
+        {
+            if (a[i] >= key && a[i-1] < key)
+                break;
+        }
+        for(int j = *n-1; j >= i; j--)
+        {
+            a[j+1] = a[j];
+        }
+        a[i] = key;
     }
-    a[i+1]=m;
-    n++;
+    (*n)++;
 }
 
+int main(){
+    int a[100];
+    int n;
+    printf("1.Nhap gia tri n phan tu cua mang:\n");
+    printf("2.In gia tri cac phan tu trong mang:\n");
+    printf("3.Tinh trung binh cac phan tu duong trong mang:\n");
+    printf("4.In ra vi tri cac phan tu co gia tri bang k (k nhap tu ban phim) trong mang:\n");
+    printf("5.Su dung thuat toan sap xep noi bot sap xep mang giam dan:\n");
+    printf("6.Tinh so luong cac phan tu la so nguyen to trong mang:\n");
+    printf("7.Sap xep cac phan tu chan chia het cho 3:\n");
+    printf("8.Nhap gia tri m tu ban phim, chen gia tri m vao mang (sap xeo giam dan) dung vi tri:\n");
+    printf("9.Thoat\n");
+    int choice;
 
-
-
-int main () {
-    int Chon, n;
-    float m, my_array;
-    do {
-        printf("1.Nhap gia tri n phan tu cua mang:\n");
-        printf("2.In ra gia tri cac phan tu trong mang:\n");
-        printf("3.Tinh trung binh cac phan tu duong(>0) trong mang:\n");
-        printf("4.In ra vi tri (chi so) cac phan tu co gia tri bang k trong mang (k nhap tu ban phim):\n");
-        printf("5.Su dung thuat toan sap xep noi bot sap xep mang giam dan:\n");
-        printf("6.Tinh so luong cac phan tu la so nguyen trong mang: \n");
-        printf("7.Sap xep cac phan tu chan san chia het cho 3 o dau mang theo thu tu tang dan, cac phan tu le chia het cho 3 o cuoi mang theo thu tu giam dan, cac phan tu con lai ơ gia giua theo thu tu tang gian:\n");
-        printf("8.Nhap gia tri m tu ban phim, chen gia tri m vao mang(sap xep giam dan) theo dung vi tri:\n");
-        printf("9.Thoat\n");
-
-        printf("Nhap lua chon cua ban: ");
-        scanf("%d", &Chon);
-
-        switch (Chon) {
+    while (1)
+    {
+        int choice, Total, bienDem, k, count, j, b, found, m ;
+        float trungBinhCong;
+        printf("Nhap so ban muon chon la: ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
             case 1:
-                Nhapgiatrin();
+                printf("Gia tri phan tu n cua mang la:");
+                scanf("%d", &n);
                 break;
             case 2:
-                ingiatri();
-                break;
-            case 3:
-                averagePositive();
-                break;
-            case 4:
-                if (cout > 0) {
-                    int k;
-                    printf("Nhap gia tri k can tim trong mang: ");
-                    scanf("%d", &k);
-                    giatrik(k);
-                } else {
-                    printf("Mang chua duoc nhap! Vui long nhap mang truoc.\n");
+                for(int i=0; i<n; i++)
+                {
+                    printf("Nhap gia tri phan tu thu %d:", i+1);
+                    scanf("%d", &a[i]);
                 }
-                break;
-            case 6:
-                printf("So luong cac so nguyen to trong mang la: %d\n", count_primes(&my_array, n));
-                break;
-            case 8:
-                printf("Nhap gia tri m: ");
-                scanf("%d",&m);
-
-                chenSo(m);
-                printf("Mang sau khi chen: \n");
-                for (int i = 0; i < n; i++) {
+                printf("\nGia tri cac phan tu trong mang la:\n");
+                for(int i=0; i<n; i++)
+                {
                     printf("%d\n", a[i]);
                 }
                 break;
+            case 3:
+                Total = 0;
+                bienDem = 0;
+                for(int i=0; i<n; i++)
+                {
+                    if (a[i] > 0)
+                    {
+                        Total += a[i];
+                        bienDem++;
+                    }
+                }
+                if (bienDem > 0)
+                {
+                    trungBinhCong = (float)Total / bienDem;
+                    printf("Trung binh cac phan tu duong trong mang = %f\n", trungBinhCong);
+                }else
+                    printf("Khong co phan tu duong nao trong mang.\n");
+                break;
+            case 4:
+                printf("Nhap gia tri k:");
+                scanf("%d", &k);
+                found = 0;
+                for(int i=0; i<n; i++)
+                {
+                    if(a[i] == k)
+                    {
+                        printf("Cac vi tri co gia tri = %d la: %d\n", k, i);
+                        found++;
+                    }
+                }
+                if(!found)
+                    printf("Khong co vi tri nao!\n");
+                break;
+            case 5:
+                bubbleShort(a, n);
+                printf("Mang sau khi duoc sap xep theo thu tu giam dan: \n");
+                array_tent(a, n);
+                break;
+            case 6:
+                count = 0;
+                printf("Danh sach cac so nguyen to la: ");
+                for (int i=0; i<n; i++)
+                {
+                    j = 2;
+                    b = 1;
+                    while (j < a[i])
+                    {
+                        if(a[i] % j == 0)
+                            b = 0;
+                        break;
+                        j++;
+                    }
+                    if(b == 1)
+                    {
+                        count = count + 1;
+                        printf("%d ", a[i]);
+                    }
+                }
+                printf("\nSo luong so nguyen to trong mang la: %d\n", count);
+                break;
+            case 8:
+                printf("Nhap gia tri cua m:");
+                scanf("%d", &m);
+                chenSo(a, &n, m);
+
+                printf("Mang sau khi duoc chen gia tri m: ");
+                for(int i=0; i<n; i++) {
+                    printf("%d ", a[i]);
+                } printf("\n");
+                break;
             case 9:
-                printf("Ket thu chuong trinh\n");
-                break;
+                printf("Thoat khoi chuong trinh: ");
             default:
-                printf("Khong co su lua chon nay. Moi ban nhap lai lua chon:\n");
-                break;
+                printf("khong co su lua chon nay: ");
         }
-    } while (Chon != 9);
+    }while (choice !=9);
     return 0;
 }
