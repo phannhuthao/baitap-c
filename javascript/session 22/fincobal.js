f2bb16b2d90f5e4959efc423e5f00ff3379a9466
phannhuthao <149989588+phannhuthao@users.noreply.github.com>
2024-02-26 08:46:20 +0700

    Add files via upload

---
+++ javascript/session 22/fincobal.js
@@ -0,0 +1,15 @@
+let n = Number(prompt("Nhập một số nguyên dương N vào: "));
+
+function fibonacci(n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+if (n < 0) {
+    console.log("Vui lòng nhập số nguyên dương.");
+} else {
+    console.log("Số Fibonacci thứ", n, " là:", fibonacci(n));
+}
+
